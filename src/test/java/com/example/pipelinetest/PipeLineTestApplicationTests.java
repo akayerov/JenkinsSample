7428362d57c709d3f4a8daa556d00da860811e54
Akayerov <akayerov@yandex.ru>
2024-09-01 05:17:08 +0300

    initial commit

---
+++ src/test/java/com/example/pipelinetest/PipeLineTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pipelinetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipeLineTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
